6c2d21a9f98556468d4f1da5e34256fdfb2f5d1b
adeshpokhrel <apokhrel@inventechgroup.com>
2023-08-03 12:52:35 +0545

    login page with error handling in spring boot

--- src/main/java/com/MVC/example/SpringMVCSecurity/Controller/LoginController.java
+++ src/main/java/com/MVC/example/SpringMVCSecurity/Controller/LoginController.java
@@ -1,16 +1,12 @@
 package com.MVC.example.SpringMVCSecurity.Controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
-public class DemoController  {
-    @GetMapping("/")
-    public String showHome(){
-        return "home";
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+        return "plain-login";
     }
-
-
 }
